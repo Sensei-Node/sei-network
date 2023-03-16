9a1b0a017cf0389a32bf7d007a3a0d11a97ae5aa
Ubuntu <ubuntu@sensei-seinetwork-validator-testnet.southamerica-west1-a.c.senseicloudinfrastructure.internal>
2023-03-16 14:36:01 +0000

    remove snap sync, we will include it via .env

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SNAPSHOT_FILE=$(curl -Ls https://snapshots.brocha.in/sei-testnet-2/atlantic-2.json | jq -r .goleveldb.file)
-curl -L https://snapshots.brocha.in/sei-testnet-2/$SNAPSHOT_FILE | lz4 -dc - | tar -xf - -C ~/.sei
-curl https://raw.githubusercontent.com/sei-protocol/testnet/master/atlantic-2/genesis.json > ~/.sei/config/genesis.json
+#SNAPSHOT_FILE=$(curl -Ls https://snapshots.brocha.in/sei-testnet-2/atlantic-2.json | jq -r .goleveldb.file)
+#curl -L https://snapshots.brocha.in/sei-testnet-2/$SNAPSHOT_FILE | lz4 -dc - | tar -xf - -C ~/.sei
+#curl https://raw.githubusercontent.com/sei-protocol/testnet/master/atlantic-2/genesis.json > ~/.sei/config/genesis.json
 
 cp /sei-config/config.toml ~/.sei/config/config.toml
 cp /sei-config/client.toml ~/.sei/config/client.toml
@@ -12,4 +12,3 @@ cp /sei-config/client.toml ~/.sei/config/client.toml
 #sed -i.bak -e "s|^chain-id *=.*|chain-id = $CHAIN_ID|" ~/.sei/config/client.toml
 
 seid start
-seid status
