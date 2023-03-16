ae632b19fab66c1659349a40914bf520229e0d9d
carloscip <98537833+carloscip@users.noreply.github.com>
2023-03-16 14:55:04 +0000

    hability to opt-out of snapsync from .env, modify config variables from .env too

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+if [ "$SNAP_SYNC" != "" ]; then
+    if [ "$CHAIN_ID" == "atlantic-2" ]; then
+        SNAPSHOT_FILE=$(curl -Ls https://snapshots.brocha.in/sei-testnet-2/atlantic-2.json | jq -r .goleveldb.file)
+        curl -L https://snapshots.brocha.in/sei-testnet-2/$SNAPSHOT_FILE | lz4 -dc - | tar -xf - -C ~/.sei
+    fi
+fi
 
-#SNAPSHOT_FILE=$(curl -Ls https://snapshots.brocha.in/sei-testnet-2/atlantic-2.json | jq -r .goleveldb.file)
-#curl -L https://snapshots.brocha.in/sei-testnet-2/$SNAPSHOT_FILE | lz4 -dc - | tar -xf - -C ~/.sei
-#curl https://raw.githubusercontent.com/sei-protocol/testnet/master/atlantic-2/genesis.json > ~/.sei/config/genesis.json
+if [ "$GET_GENESIS" != "" ]; then
+    curl https://raw.githubusercontent.com/sei-protocol/testnet/master/$CHAIN_ID/genesis.json > ~/.sei/config/genesis.json
+fi
 
 cp /sei-config/config.toml ~/.sei/config/config.toml
 cp /sei-config/client.toml ~/.sei/config/client.toml
 
-#sed -i.bak -e "s|^moniker *=.*|moniker = $MONIKER|" ~/.sei/config/config.toml
-#sed -i.bak -e "s|^mode *=.*|mode = $MODE|" ~/.sei/config/config.toml
-#sed -i.bak -e "s|^chain-id *=.*|chain-id = $CHAIN_ID|" ~/.sei/config/client.toml
+# NEED TO TEST THESE COMMANDS
+sed -i "s/moniker = \".*\"/moniker = \"$MONIKER\"/" ~/.sei/config/config.toml
+sed -i "s/mode = \".*\"/mode = \"$MODE\"/" ~/.sei/config/config.toml
+sed -i "s/chain-id = \".*\"/chain-id = \"$CHAIN_ID\"/" ~/.sei/config/client.toml
 
 seid start
