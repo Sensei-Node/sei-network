739b83cc7b4fb2e7ae8beafd3bd7666a91b6ac2f
carloscip <98537833+carloscip@users.noreply.github.com>
2023-03-17 14:29:55 +0000

    fix jq that was wrong in official docs

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,11 +12,11 @@ if [ "$STATE_SYNC" != "" ]; then
         rm -rf ~/.sei/data/*
         mv ~/.sei/priv_validator_state.json.backup ~/.sei/data/priv_validator_state.json
         
-        STATE_SYNC_RPC=https://sei-testnet-2-rpc.brocha.in:443
+        STATE_SYNC_RPC=https://sei-testnet-2-rpc.brocha.in
         STATE_SYNC_PEER=94b63fddfc78230f51aeb7ac34b9fb86bd042a77@sei-testnet-2.p2p.brocha.in:30588
-        LATEST_HEIGHT=$(curl -s $STATE_SYNC_RPC/block | jq -r .result.block.header.height)
+        LATEST_HEIGHT=$(curl -s $STATE_SYNC_RPC/block | jq -r .block.header.height)
         SYNC_BLOCK_HEIGHT=$(($LATEST_HEIGHT - 2000))
-        SYNC_BLOCK_HASH=$(curl -s "$STATE_SYNC_RPC/block?height=$SYNC_BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+        SYNC_BLOCK_HASH=$(curl -s "$STATE_SYNC_RPC/block?height=$SYNC_BLOCK_HEIGHT" | jq -r .block_id.hash)
 
         sed -i.bak -e "s|^enable *=.*|enable = true|" ~/.sei/config/config.toml
         sed -i.bak -e "s|^rpc-servers *=.*|rpc-servers = \"$STATE_SYNC_RPC,$STATE_SYNC_RPC\"|" ~/.sei/config/config.toml
